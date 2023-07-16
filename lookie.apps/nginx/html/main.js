2959452e1a610554bef335d49803dab06b9d65d1
Itameshi <itameshi.m@gmail.com>
2023-07-16 10:59:58 +0900

    UnsupportedMediaTypeの回避
    POSTで受け取った値をJavaScriptからJSON形式でDjangoのバックエンドに投げる際
    UnsupportedMediaTypeエラーで受け取れなかったので修正。

--- lookie.apps/nginx/html/main.js
+++ lookie.apps/nginx/html/main.js
@@ -4,81 +4,54 @@ const taskAddButtonElement = document.getElementById('task-add-button')
 
 const apiEndpoint = 'http://localhost:8001/api/tasks'
 
-/*
 async function loadTasks() {
-	const response = await fetch('/api/tasks')
-	const responseBody = await response.json()
-	
-	const tasks = responseBody.tasks
-
-	// 追加ボタンを押したタイミングでタスク欄に表示されるようにする
-	while (tasksTableBodyElement.firstChild) {
-		tasksTableBodyElement.removeChild(tasksTableBodyElement.firstChild)
-	}
-
-	tasks.forEach((task) => {
-		const titleTdElement = document.createElement('td')
-		titleTdElement.innerText = task.title
-
-		const createdAtTdElement = document.createElement('td')
-		createdAtTdElement.innerText = task.createdAt
-
-		const trElement = document.createElement('tr')
-		trElement.appendChild(titleTdElement)
-		trElement.appendChild(createdAtTdElement)
-
-		tasksTableBodyElement.appendChild(trElement)
-	})
-}
-
-async function registerTask() {
-	const title = taskTitleInputElement.value
-	
-	const requestBody = {
-		title: title
-	}
-	await fetch('/api/tasks', {
-		method: 'POST',
-		body: JSON.stringify(requestBody)
-	})
-
-	await loadTasks()
-}
-*/
-
-// jsonの取得テスト
-async function TasksLoadTest(){
-  //const response = await fetch('/api/tasks')
-  const response = await fetch(`${apiEndpoint}`);
-  //console.log(`${apiEndpoint}/api/tasks`)
-  //console.log(response)
-  //const responseBody = await response.json()
-  /*
+  const response = await fetch(`${apiEndpoint}`) 
   const responseBody = await response.json()
 
-  const tasks = responseBody.tasks
+  // 追加ボタンを押したタイミングでタスク欄に表示されるようにする
+  while (tasksTableBodyElement.firstChild) {
+    tasksTableBodyElement.removeChild(tasksTableBodyElement.firstChild)
+  }
 
-  tasks.forEach((task) => {
+  responseBody.forEach((task) => {
     const titleTdElement = document.createElement('td')
     titleTdElement.innerText = task.title
 
     const createdAtTdElement = document.createElement('td')
-    createAtTdElement.innerText = task.createdAt
+    createdAtTdElement.innerText = task.createdAt
 
     const trElement = document.createElement('tr')
     trElement.appendChild(titleTdElement)
-    trElement.appendChild(createAtTdElement)
+    trElement.appendChild(createdAtTdElement)
 
     tasksTableBodyElement.appendChild(trElement)
   })
-  */
 }
 
-async function main(){
-  TasksLoadTest()
-	//taskAddButtonElement.addEventListener('click', registerTask)
-	//await loadTasks()
+async function registerTask() {
+  const title = taskTitleInputElement.value
 
+  const requestBody = {
+  	title: title
+  }
+  console.log(JSON.stringify(requestBody))
+  await fetch(`${apiEndpoint}`, {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestBody)
+  })
+  console.log('===========================================================')
+
+	await loadTasks()
+}
+
+
+async function main(){
+  //TasksLoadTest()
+	taskAddButtonElement.addEventListener('click', registerTask)
+	await loadTasks()
 }
 
 main()
